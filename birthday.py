070079ccf01983f0972597fce40f2ebc20c16e43
Webster Avosa <31823892+avosa@users.noreply.github.com>
2020-06-22 00:05:54 +0300

    Update birthday.py

--- birthday.py
+++ birthday.py
@@ -4,13 +4,13 @@ month = datetime.date.today().month
 day = datetime.date.today().day
 
 if month == 6:
-    if day < 22:
+    if day < 24:
         print("Ooops, you are not 23 yet!")
-    elif day > 22:
+    elif day > 24:
         print("You are past your birthday!")
     else:
         print("Webster, it's your birthday! You're turning a year older today!")
-elif month < 4:
+elif month < 6:
     print("It's not your birth month!")
 else:
     print("It's past your birth month!")
